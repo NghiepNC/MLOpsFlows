818ad66c81c41888f20f9ebb5e50db465dc7a5d7
namnd00 <nam.nd.00@gmail.com>
2023-03-17 16:45:10 +0700

    add test

---
+++ tests/data_pipeline/fake/goodreads/test_generate_fake_data.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   test_generate_fake_data.py
+@Time    :   2023/03/17 16:23:58
+@Author  :   Duy Nam
+@Version :   0.1
+@Contact :   nam.nd.00@gmail.com
+@Desc    :   MLOps Flows
+'''
